b58fdd2fea722368177fb2a75c062cc07f9bf557
Blake Spangenberg <toasterparty@derpymail.org>
2021-12-10 19:04:15 -0800

    update clean.sh

--- tools/clean.sh
+++ tools/clean.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 rm -rf $SCRIPT_DIR/venv
-
+rm -rf $SCRIPT_DIR/../build
+rm -f $SCRIPT_DIR/../src/mp2hudcolor_wrapper.c
+rm -f $SCRIPT_DIR/../*.so
