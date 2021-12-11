c9cd804ad23cf64a26b235034bc63f403e04c87c
Blake Spangenberg <toasterparty@derpymail.org>
2021-12-10 16:14:50 -0800

    added venv script

---
+++ tools/venv.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+PYTHON=$SCRIPT_DIR/venv/bin/python3
+
+echo "Creating Virtual Enviornment..."
+python3 -m venv $SCRIPT_DIR/venv
+echo "Updating pip..."
+$PYTHON -m pip install --upgrade pip
+echo "Installing requirements..."
+$PYTHON -m pip install -r $SCRIPT_DIR/../requirements.txt
