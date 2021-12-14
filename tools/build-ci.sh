d4ed6c2e9a65ea6bf0ac39fcf45e5e798a471174
Blake Spangenberg <blake.spangenberg1@gmail.com>
2021-12-14 15:45:04 -0800

    .

--- tools/build-ci.sh
+++ tools/build-ci.sh
@@ -6,7 +6,7 @@ set -x
 echo "Adding $PYTHON_VERSION to path"
 export PATH="/opt/python/${PYTHON_VERSION}/bin:$PATH"
 
-python -m pip install --use-feature=2020-resolver auditwheel "setuptools>=44.0.0" "wheel>=0.34.2" "Cython>=0.29.21" "setuptools_scm[toml]>=3.4"
+python -m pip install auditwheel "setuptools>=44.0.0" "wheel>=0.34.2" "Cython>=0.29.21" "setuptools_scm[toml]>=3.4"
 python setup.py bdist_wheel
 
 python -m auditwheel repair --plat manylinux2010_x86_64 dist/nod-*-linux_x86_64.whl -w dist
