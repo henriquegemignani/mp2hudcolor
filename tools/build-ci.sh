b8b6fafd8c7e7cc8bdc84d003a9568c27a04e413
Blake Spangenberg <blake.spangenberg1@gmail.com>
2022-01-11 13:26:12 -0800

    Python 3.9

--- tools/build-ci.sh
+++ tools/build-ci.sh
@@ -6,10 +6,10 @@ set -x
 echo "Adding $PYTHON_VERSION to path"
 export PATH="/opt/python/${PYTHON_VERSION}/bin:$PATH"
 
-python -m pip install auditwheel "setuptools>=44.0.0" "wheel>=0.34.2" "Cython>=0.29.21" "setuptools_scm[toml]>=3.4"
-python setup.py bdist_wheel
+python3.9 -m pip install auditwheel "setuptools>=44.0.0" "wheel>=0.34.2" "Cython>=0.29.21" "setuptools_scm[toml]>=3.4"
+python3.9 setup.py bdist_wheel
 
-python -m auditwheel repair --plat manylinux2010_x86_64 dist/*-linux_x86_64.whl -w dist
+python3.9 -m auditwheel repair --plat manylinux2010_x86_64 dist/*-linux_x86_64.whl -w dist
 rm dist/*-linux_x86_64.whl
 
 echo "Resulting files: "
