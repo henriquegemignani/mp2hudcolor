6addf1ca3f590e323ced447b2756fd5a92389c43
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2022-08-02 19:25:43 +0300

    Add setup.cfg

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 from setuptools import setup, Extension
 from Cython.Distutils import build_ext
 
@@ -10,22 +9,7 @@ extensions = [
     Extension("mp2hudcolor", src)
 ]
 
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
-
 setup(
-    name="mp2hudcolor",
-    version="1.0.11",
-    description="Modifies an existing NTWK file for Metroid Prime 2: Echoes and changing the color of the HUD.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/toasterparty/mp2hudcolor",
-    author="toasterparty",
-    author_email="toasterparty@derpymail.org",
-    license="MIT",
-    packages=['mp2hudcolor'],
-    install_requires=[
-    ],
     ext_modules=extensions,
     cmdclass={'build_ext': build_ext},
     package_data={'mp2hudcolor': ['mp2hudcolor.c']}
