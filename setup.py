559c0ab3e4a36ee5c63defca166e347adabdc449
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-03-29 17:56:01 +0300

    Clean bitrot, enable limited api

--- setup.py
+++ setup.py
@@ -6,11 +6,21 @@ src = [
 ]
 
 extensions = [
-    Extension("mp2hudcolor", src)
+    Extension(
+        "mp2hudcolor",
+        src,
+        py_limited_api=True,
+        define_macros=[("Py_LIMITED_API", "0x030a0000")],
+    )
 ]
 
 setup(
     ext_modules=extensions,
     cmdclass={'build_ext': build_ext},
-    package_data={'mp2hudcolor': ['mp2hudcolor.c']}
+    package_data={'mp2hudcolor': ['mp2hudcolor.c']},
+    options={
+        "bdist_wheel": {
+            "py_limited_api": "cp310",
+        }
+    },
 )
