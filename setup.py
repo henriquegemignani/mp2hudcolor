cb2fb62533ca9f458885667f63a8f3a6662130f7
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-03-29 18:24:17 +0300

    Fix wheel

--- setup.py
+++ setup.py
@@ -1,23 +1,17 @@
 from setuptools import setup, Extension
-from Cython.Distutils import build_ext
-
-src = [
-    'mp2hudcolor/mp2hudcolor_wrapper.pyx'
-]
+from Cython.Build import cythonize
 
 extensions = [
     Extension(
         "mp2hudcolor",
-        src,
+        ['mp2hudcolor/mp2hudcolor_wrapper.pyx'],
         py_limited_api=True,
         define_macros=[("Py_LIMITED_API", "0x030a0000")],
     )
 ]
 
 setup(
-    ext_modules=extensions,
-    cmdclass={'build_ext': build_ext},
-    package_data={'mp2hudcolor': ['mp2hudcolor.c']},
+    ext_modules=cythonize(extensions),
     options={
         "bdist_wheel": {
             "py_limited_api": "cp310",
